4fcefec4344f99b55ee9a3a81f406eb58f9ad496
Anton <62605227+AntonUA86@users.noreply.github.com>
2021-01-13 17:41:07 +0300

    Add files via upload

---
+++ Lesson 12/12_1/Lines.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    static class Lines
+    {
+    
+
+        public static bool VerticalLeft(int size ,int column, int row )
+        {
+            if (column == 0)
+                return true;
+            return false;
+
+        }
+        public static bool VerticalRight(int size, int column, int row)
+        {
+            if (column == size - 1)
+                return true;
+            return false;
+        }
+        public static bool HorizontalTop(int size, int column, int row)
+        {
+            if (row == 0)
+                return true;
+            return false;
+        }
+        public static bool HorizontalBottom(int size, int column, int row)
+        {
+            if (row == (size - 1))
+                return true;
+            return false;
+        }
+        public static bool Slash(int size, int column, int row)
+        {
+            if (column == size - row - 1)
+                return true;
+            return false;
+        }
+        public static bool DiagonalRow(int size, int column, int row)
+        {
+            if (column == row)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Lesson 12/12_1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lesson
+{
+    delegate void PrintLatter(int size, int column, int row);
+    delegate bool Line(int size, int column, int row);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Line[] N = new Line[]
+         {
+             new Line(Lines.VerticalLeft),
+             new Line(Lines.VerticalRight),
+             new Line(Lines.DiagonalRow),
+         };
+
+            Line[] O = new Line[]
+        {
+                new Line(Lines.HorizontalTop),
+                new Line(Lines.HorizontalBottom),
+                new Line(Lines.VerticalLeft),
+                new Line(Lines.VerticalRight)
+        };
+
+            Line[] Z = new Line[]
+            {
+                new Line(Lines.Slash),
+                new Line(Lines.HorizontalBottom),
+                new Line(Lines.HorizontalTop)
+            };
+
+            Line[] X = new Line[]
+            {
+                new Line(Lines.DiagonalRow),
+                new Line(Lines.Slash)
+            };
+      
+
+           PrintLatter(9, N);
+           PrintLatter(9, Z);
+           PrintLatter(9, X);
+           PrintLatter(9, O);
+
+            Console.Read();
+        }
+
+
+        static void PrintLatter(int size, Line[] letter)
+        {
+            for (int row = 0; row < size; row++)
+            {
+                for (int column = 0; column < size; column++)
+                {
+                    for (int index = 0; index < letter.Length; index++)
+                    {
+                        if (letter[index](size , column ,row))
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                    if (row == 0 || row == (size - 1))
+                        Console.Write("");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
+
+
