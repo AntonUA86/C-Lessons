76526c1bdf9d94caacad994735552e41debbd39a
Anton <62605227+AntonUA86@users.noreply.github.com>
2021-01-22 14:47:34 +0300

    Add files via upload

---
+++ Lesson 13/13_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*У вас есть класс студент Товар… вам необходимо попробывать разную реализацию метода
+Clone (глубокое/поверхностое клонирование)…. У студента Товар есть поле Адрес –это
+класс а у Адресса есть поле City, city – это тоже класс.*/
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Student
+    {
+        Adress adress
+
+    }
+    class Goods
+    {
+
+    }
+    class Adress
+    {
+        
+    }
+
+    class City 
+    {
+
+    }
+}
+
+
+
