4d5418f1082a81f6ce74ee541bf60547d298d858
Anton <62605227+AntonUA86@users.noreply.github.com>
2021-03-31 08:54:28 +0300

    Add files via upload

---
+++ Lesson 19/19_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/*Напишите регулярку для ввода числа от 0 до 99999...Обратите внимание что число 00 или
+01 не должно быть валидным…..*/
+
+namespace Lesson
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            
+            Regex regex = new Regex(@"^([1-9][0-9]{0,2}|999)$");
+
+
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                int number = int.Parse(Console.ReadLine());
+                MatchCollection match = regex.Matches(Convert.ToString(number));
+                if (match.Count > 0)
+                    Console.WriteLine("Правильно");
+                else
+                    Console.WriteLine("Нет");
+            }
+
+            
+        }
+    }
+
+}
---
+++ Lesson 19/19_2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+
+            IsolatedStorageFile userStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+            userStorage.CreateFile("Gmail.txt");
+            string[] files = userStorage.GetFileNames("Gmail.txt");
+
+            using (StreamWriter sw = new StreamWriter(files[0], true, Encoding.Default))
+             {
+                 string[] label = { "gmail.com", "yandex.ru", "ukr.net" };
+
+                 for (int i = 0; i < 1000000; i++)
+                 {
+                     for (int z = 0; z < 3; z++)
+                     {
+                         StringBuilder gmail = new StringBuilder($"https://@{label[z]}");
+                         gmail.Insert(8, Convert.ToChar(rnd.Next('A', 'Z')))
+                                          .Insert(9, Convert.ToByte(rnd.Next('0', '9')))
+                                          .Insert(10, Convert.ToChar(rnd.Next('a', 'z')))
+                                          .Insert(11, Convert.ToChar(rnd.Next('A', 'Z')))
+                                          .Insert(12, Convert.ToByte(rnd.Next('0', '9')));
+
+
+
+                         sw.WriteLine(gmail.ToString());
+                         Console.WriteLine($"Количество записей = {i}");
+                     }
+                 }
+
+             }
+
+            
+            using (StreamReader sr = new StreamReader(files[0]))
+            {
+
+                Regex regexGmail = new Regex(@"gmail", RegexOptions.IgnoreCase);
+                MatchCollection arr = regexGmail.Matches(sr.ReadToEnd());
+                Console.WriteLine($"Почтовых Ящиков Gmail : {arr.Count}");
+            }
+            using (StreamReader sr = new StreamReader(files[0]))
+            {
+                Regex regexUkr = new Regex(@"ukr", RegexOptions.IgnoreCase);
+                MatchCollection arr =  regexUkr.Matches(sr.ReadToEnd());           
+                Console.WriteLine($"Почтовых Ящиков ukr : {arr.Count}");
+            }
+            using (StreamReader sr = new StreamReader(files[0]))
+            {
+                Regex regexYandex = new Regex(@"yandex", RegexOptions.IgnoreCase);
+                MatchCollection arr =regexYandex.Matches(sr.ReadToEnd());
+                Console.WriteLine($"Почтовых Ящиков Yandex : {arr.Count}");
+            }
+            Console.Read();
+        }
+    }
+}
