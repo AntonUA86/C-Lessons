521c7dc27e607745d0f872bda46b43188dfc481d
Anton <62605227+AntonUA86@users.noreply.github.com>
2021-03-31 08:53:59 +0300

    Add files via upload

---
+++ Lesson 17/17_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lesson
+{
+    /*   1. Создайте коллекцию продуктов со следующими данными: Код товара, название товара,тип
+     товара, цена…. Используя LINQ запрос, выведите на экран код товара с ценой выше 30.
+   
+    */
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Product> catlog = new List<Product>();
+            catlog.Add(new Product(3, "Баклажан", "Овощи", 20));
+            catlog.Add(new Product(25, "Мыло", "Химия", 123));
+            catlog.Add(new Product(34, "Шампунь", "Химия", 200));
+            catlog.Add(new Product(2, "Банан", "Фрукты", 55));
+            catlog.Add(new Product(22, "Джек Дэниэлс", "Алкоголь", 560));
+            catlog.Add(new Product(23, "Мандарин", "Фрукты", 69));
+
+
+            var query =
+                from product in catlog
+                where product.Cost > 30
+                select new
+                {
+                    CodeProduct = product.CodeProduct,
+                    Name = product.Name,
+                    TypeProduct = product.TypeProduct,
+                    Cost = product.Cost
+                };
+          
+
+            Console.WriteLine("Список продуктов :\n");
+            foreach (var item in query)
+            {
+                Console.WriteLine("Код :{0} Имя :{1} Тип :{2}  Цена:{3}", item.CodeProduct, item.Name, item.TypeProduct, item.Cost);
+            }
+            Console.Read();
+
+        }
+
+        class Product
+        {
+
+            public int CodeProduct { get; set; }
+            public string Name { get; set; }
+            public string TypeProduct { get; set; }
+            public int Cost { get; set; }
+
+            public Product(int CodeProduct, string Name, string TypeProduct, int Cost)
+            {
+                this.CodeProduct = CodeProduct;
+                this.Name = Name;
+                this.TypeProduct = TypeProduct;
+                this.Cost = Cost;
+            }
+
+        }
+    }
+}
+
+
+
+
---
+++ Lesson 17/17_2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<СarOwner> carOwner = new List<СarOwner>();
+            carOwner.Add(new СarOwner("Mercedes-Benz", "Vlad", 0956253232));
+            carOwner.Add(new СarOwner("Renault", "Mark", 0256253331));
+            carOwner.Add(new СarOwner("Mercedes-Benz", "Anton", 0956353235));
+            carOwner.Add(new СarOwner("Peugeot", "Olha", 0953252323));
+            carOwner.Add(new СarOwner("Hyundai", "Igor", 0753232399));
+
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car("Mercedes-Benz", 2020, "Black"));
+            cars.Add(new Car("Peugeot", 2018, "Red"));
+            cars.Add(new Car("Hyundai", 2016, "Black"));
+            cars.Add(new Car("BMW", 2021, "Green"));
+            cars.Add(new Car("Renault", 2014, "White"));
+
+
+
+
+            var ListClients = carOwner.OrderBy(i => i.Name)
+                                      .Join(cars,
+                                            client => client.Brand,
+                                            car => car.Brand,
+                                           (client, car) => new
+                                           {
+                                               Name = client.Name,
+                                               Phone = client.NumberPhone,
+                                               Model = car.Brand,
+                                               YearOfIssue = car.YearOfIssue,
+                                               Color = car.Color
+                                           });
+
+            foreach (var clients in ListClients)
+            {
+                Console.WriteLine($"Имя: {clients.Name} Телефон: {clients.Phone} Модель: {clients.Model} Год Выпуска: {clients.YearOfIssue} Цвет: {clients.Color}");
+            }
+
+            Console.Read();
+
+        }
+
+
+
+    }
+
+    class Car
+    {
+        public string Brand { get; set; }
+        public int YearOfIssue { get; set; }
+        public string Color { get; set; }
+
+        public Car(string Brand, int YearOfIssue, string Color)
+        {
+            this.Brand = Brand;
+            this.YearOfIssue = YearOfIssue;
+            this.Color = Color;
+        }
+
+
+    }
+
+    class СarOwner
+    {
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public int NumberPhone { get; set; }
+
+        public СarOwner(string Brand, string Name, int NumberPhone)
+        {
+            this.Brand = Brand;
+            this.Name = Name;
+            this.NumberPhone = NumberPhone;
+        }
+    }
+
+
+}
+
